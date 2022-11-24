fc6712c8a0bac2cdba023c60d4432138ffc5699a
SURLE JONATHAN <j.surle@acropole-expert.com>
2022-11-24 18:21:57 +0100

    add router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Layout from "./components/Layout";
+import Single from "./pages/Single";
+import Write from "./pages/Write";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/post/:id",
+        element: <Single />,
+      },
+      {
+        path: "/write",
+        element: <Write />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
+
 const App = () => {
   return (
     <div>
-      Hello World!
+      <RouterProvider router={router} />
     </div>
-  )
-}
+  );
+};
 
 export default App;
