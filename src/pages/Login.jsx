efb80f6027855b14f8499d1db33e9300ad3e0ce2
surlejonathan <surle.jonathan@gmail.com>
2022-12-01 23:36:49 +0100

    connect Login and Register pages to API

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,54 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
+  const [inputValues, setInputValues] = useState({
+    username: "",
+    password: "",
+  });
+  const { username, password } = inputValues;
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setInputValues((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post(`/auth/login`, inputValues);
+      setSuccess("You are successfully signed in");
+      setError("");
+      navigate("/");
+    } catch (error) {
+      setError(error.response.data);
+    }
+  };
   return (
     <div className='auth'>
       <h1>Login</h1>
-      <form>
-        <input type='text' placeholder='username' />
-        <input type='password' placeholder='password' />
-        <button>Login</button>
-        <p>There is an error !</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='username'
+          name='username'
+          value={username}
+          onChange={handleChange}
+        />
+        <input
+          type='password'
+          placeholder='password'
+          name='password'
+          value={password}
+          onChange={handleChange}
+        />
+        <button type='submit'>Login</button>
+        {error && <p>{error}</p>}
+        {success && <p className='success-message'>{success}</p>}
         <span>
           No account yet ? <Link to='/register'>Sign up now !</Link>
         </span>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,16 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Register = () => {
+  const [inputValues, setInputValues] = useState({
+    username: "",
+    password: "",
+    email: "",
+  });
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  const { username, password, email } = inputValues;
+
+  const handleChange = (e) => {
+    setInputValues((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post(`/auth/register`, inputValues);
+      setSuccess("Your account has been successfully created");
+      setError(false);
+    } catch (error) {
+      setError(error.response.data);
+    }
+  };
+
   return (
     <div className='auth'>
       <h1>Sign up</h1>
-      <form>
-        <input required type='text' placeholder='username' />
-        <input required type='email' placeholder='email' />
-        <input required type='password' placeholder='password' />
-        <button>Register</button>
-        <p>There is an error !</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          required
+          type='text'
+          name='username'
+          placeholder='username'
+          value={username}
+          onChange={handleChange}
+        />
+        <input
+          required
+          type='email'
+          name='email'
+          placeholder='email'
+          value={email}
+          onChange={handleChange}
+        />
+        <input
+          required
+          type='password'
+          name='password'
+          placeholder='password'
+          value={password}
+          onChange={handleChange}
+        />
+        <button type='submit'>Register</button>
+        {error && <p>{error}</p>}
+        {success && <p className='success-message'>{success}</p>}
         <span>
           Have already an account ? <Link to='/login'>Sign in now !</Link>
         </span>
